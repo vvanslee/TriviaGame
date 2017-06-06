ca614d4bbc85dfb2169a317294b61ee25cd73d3d
vvanslee <vvanslee@gmail.com>
2017-06-06 17:28:30 -0500

    starting over...

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,61 +1,110 @@
-// scripts here:
 
-  function submitQuiz() {
-    console.log('submitted');
+var questions = [{
+    question: "What is the population of Brazil?",
+    choices: ["145 million", "199 million", "182 million", "205 million"],
+    correctAnswer: 1
+}, {
+    question: "What is 27*14?",
+    choices: ["485", "634", "408", "528"],
+    correctAnswer: 2
+}, {
+    question: "What is the busiest train station in the world?",
+    choices: ["Grand Central, NY", "Shibuya, Tokyo", "Beijing Central, Chine", "Gard du Nord, Paris"],
+    correctAnswer: 1
+}, {
+    question: "What is the longest river?",
+    choices: ["Nile", "Amazon", "Mississippi", "Yangtze"],
+    correctAnswer: 0
+}, {
+    question: "What is the busiest tube station in the London?",
+    choices: ["Waterloo", "Baker Street", "Kings Cross", "Victoria"],
+    correctAnswer: 0
+}];
 
-  // get each answer score
-    function answerScore (qName) {
-      var radiosNo = document.getElementsByName(qName);
+var currentQuestion = 0;
+var correctAnswers = 0;
+var quizOver = false;
 
-      for (var i = 0, length = radiosNo.length; i < length; i++) {
-          if (radiosNo[i].checked) {
-      // do something with radiosNo
-          var answerValue = Number(radiosNo[i].value);
+$(document).ready(function () {
+
+    // Display the first question
+    displayCurrentQuestion();
+    $(this).find(".quizMessage").hide();
+
+    // On clicking next, display the next question
+    $(this).find(".nextButton").on("click", function () {
+        if (!quizOver) {
+
+            value = $("input[type='radio']:checked").val();
+
+            if (value == undefined) {
+                $(document).find(".quizMessage").text("Please select an answer");
+                $(document).find(".quizMessage").show();
+            } else {
+                // TODO: Remove any message -> not sure if this is efficient to call this each time....
+                $(document).find(".quizMessage").hide();
+
+                if (value == questions[currentQuestion].correctAnswer) {
+                    correctAnswers++;
+                }
+
+                currentQuestion++; // Since we have already displayed the first question on DOM ready
+                if (currentQuestion < questions.length) {
+                    displayCurrentQuestion();
+                } else {
+                    displayScore();
+                    //                    $(document).find(".nextButton").toggle();
+                    //                    $(document).find(".playAgainButton").toggle();
+                    // Change the text in the next button to ask if user wants to play again
+                    $(document).find(".nextButton").text("Play Again?");
+                    quizOver = true;
+                }
+            }
+        } else { // quiz is over and clicked the next button (which now displays 'Play Again?'
+            quizOver = false;
+            $(document).find(".nextButton").text("Next Question");
+            resetQuiz();
+            displayCurrentQuestion();
+            hideScore();
         }
-      }
-      // change NaNs to zero
-      if (isNaN(answerValue)) {
-        answerValue = 0;
-      }
-      return answerValue;
-    }
-
-  // calc score with answerScore function
-    var calcScore = (answerScore('q1') + answerScore('q2') + answerScore('q3') + answerScore('q4'));
-    console.log("CalcScore: " + calcScore); // it works!
-
-  // function to return correct answer string
-    function correctAnswer (correctStringNo, qNumber) {
-      console.log("qNumber: " + qNumber);  // logs 1,2,3,4 after called below
-      return ("The correct answer for question #" + qNumber + ": &nbsp;<strong>" +
-        (document.getElementById(correctStringNo).innerHTML) + "</strong>");
-      }
-
-  // print correct answers only if wrong (calls correctAnswer function)
-    if (answerScore('q1') === 0) {
-      document.getElementById('correctAnswer1').innerHTML = correctAnswer('correctString1', 1);
-    }
-    if (answerScore('q2') === 0) {
-      document.getElementById('correctAnswer2').innerHTML = correctAnswer('correctString2', 2);
-    }
-    if (answerScore('q3') === 0) {
-      document.getElementById('correctAnswer3').innerHTML = correctAnswer('correctString3', 3);
-    }
-    if (answerScore('q4') === 0) {
-      document.getElementById('correctAnswer4').innerHTML = correctAnswer('correctString4', 4);
-    }
-
-  // show score
-    var showScore = "ANSWERED CORRECT: " + calcScore;
-    // document.getElementById('userScore').innerHTML = showScore;
-    $('.quizScore').html(calcScore);
-
-  }
-
-  $(document).ready(function() {
-
-  $('#submitButton').click(function() {
-  $(this).addClass('hide');
-  });
+    });
 
 });
+
+// This displays the current question AND the choices
+function displayCurrentQuestion() {
+
+    console.log("In display current Question");
+
+    var question = questions[currentQuestion].question;
+    var questionClass = $(document).find(".quizContainer > .question");
+    var choiceList = $(document).find(".quizContainer > .choiceList");
+    var numChoices = questions[currentQuestion].choices.length;
+
+    // Set the questionClass text to the current question
+    $(questionClass).text(question);
+
+    // Remove all current <li> elements (if any)
+    $(choiceList).find("li").remove();
+
+    var choice;
+    for (i = 0; i < numChoices; i++) {
+        choice = questions[currentQuestion].choices[i];
+        $('<li><input type="radio" value=' + i + ' name="dynradio" />' + choice + '</li>').appendTo(choiceList);
+    }
+}
+
+function resetQuiz() {
+    currentQuestion = 0;
+    correctAnswers = 0;
+    hideScore();
+}
+
+function displayScore() {
+    $(document).find(".quizContainer > .result").text("You scored: " + correctAnswers + " out of: " + questions.length);
+    $(document).find(".quizContainer > .result").show();
+}
+
+function hideScore() {
+    $(document).find(".result").hide();
+}
